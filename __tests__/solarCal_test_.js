ea17ac52eb7a2a673f60597235d1c190d06c2768
Peter Vang <vang_peter06@yahoo.com>
2022-07-04 12:52:25 -0700

    add first test for expectancy. Should return age expectancy

--- __tests__/solarCal_test_.js
+++ __tests__/solarCal_test_.js
@@ -1,45 +1,52 @@
 import SolarCal from "../src/solarCal.js"
+import Expectancy from "../src/expectancy"
 
 describe ('SolarCal', () => {
 let newTest;
 beforeEach(() => {
-  let age = 84;
+  let age = 34;
   let expectancy = 77;
   newTest = new SolarCal(age, expectancy)
 });
 
 test("should return the age of Earth years", () => {
-  expect(newTest.earthCal()).toEqual(84);
+  expect(newTest.earthCal()).toEqual(34);
 });
 
 
 test("should return the age of Mercury years", () => {
-  expect(newTest.mercuryCal()).toEqual(20);
+  expect(newTest.mercuryCal()).toEqual(8);
 });
 
 test("should return the age of Venus years", () => {
-  expect(newTest.venusCal()).toEqual(52);
+  expect(newTest.venusCal()).toEqual(21);
 });
 
 test("should return the age of Mars years", () => {
-  expect(newTest.marsCal()).toEqual(158);
+  expect(newTest.marsCal()).toEqual(64);
 });
 
 test("should return the age of Jupiter years", () => {
-  expect(newTest.jupiterCal()).toEqual(996);
+  expect(newTest.jupiterCal()).toEqual(403);
 });
 
 test("should return the age years left to live", () => {
-  expect(newTest.expectancyCal()).toEqual(-7);
+  expect(newTest.expectancyCal()).toEqual(43);
 });
 
-test("should return number years past lives life expectancy if current age exceeds average life expectancy", () => {
-  expect(newTest.yearsYouLived()).toEqual("You have lived 7 years beyond average life expectancy");
+test("should return number years past lives life expectancy if current age exceeds average life expectancy and years you lived beyond", () => {
+  expect(newTest.yearsYouLived()).toEqual(`You have 43 years left to reach the average life expectancy`);
+});
 });
 
-test("should return number years past lives life expectancy if current age exceeds average life expectancy and years ypu lived beyond", () => {
-  expect(newTest.yearsYouLived()).toEqual("You have lived 7 years beyond average life expectancy");
-});
+describe ('Expectancy', () => {
+  let expectancyTest;
+  beforeEach(() => {
+    let region = "Amercias"
+    expectancyTest = new Expectancy(region, gender, alcohol, exercise);
+  })
 
-
-});
+  test('should return life expectancy based on user input for region', ()=> {
+    expect(expectancyTest.regionCal).toEqual(79)
+  })
+})
