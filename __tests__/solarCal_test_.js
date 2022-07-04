c8daeb79f02a5d6c389fd4be46b842dbb7c2e401
Peter Vang <vang_peter06@yahoo.com>
2022-07-04 12:15:27 -0700

    create test eight. Return age in beyond average yearand years you lived beyond

--- __tests__/solarCal_test_.js
+++ __tests__/solarCal_test_.js
@@ -9,32 +9,37 @@ beforeEach(() => {
 });
 
 test("should return the age of Earth years", () => {
-  expect(newTest.earthCal()).toEqual(34);
+  expect(newTest.earthCal()).toEqual(84);
 });
 
 
 test("should return the age of Mercury years", () => {
-  expect(newTest.mercuryCal()).toEqual(8);
+  expect(newTest.mercuryCal()).toEqual(20);
 });
 
 test("should return the age of Venus years", () => {
-  expect(newTest.venusCal()).toEqual(21);
+  expect(newTest.venusCal()).toEqual(52);
 });
 
 test("should return the age of Mars years", () => {
-  expect(newTest.marsCal()).toEqual(64);
+  expect(newTest.marsCal()).toEqual(158);
 });
 
 test("should return the age of Jupiter years", () => {
-  expect(newTest.jupiterCal()).toEqual(403);
+  expect(newTest.jupiterCal()).toEqual(996);
 });
 
 test("should return the age years left to live", () => {
-  expect(newTest.expectancyCal()).toEqual(43);
+  expect(newTest.expectancyCal()).toEqual(-7);
 });
 
 test("should return number years past lives life expectancy if current age exceeds average life expectancy", () => {
-  expect(newTest.yearsExpectancy()).toEqual("You have lived 7 years beyond average life expectancy");
+  expect(newTest.yearsYouLived()).toEqual("You have lived 7 years beyond average life expectancy");
 });
+
+test("should return number years past lives life expectancy if current age exceeds average life expectancy and years ypu lived beyond", () => {
+  expect(newTest.yearsYouLived()).toEqual("You have lived 7 years beyond average life expectancy");
+});
+
 
 });
