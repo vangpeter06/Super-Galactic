2c677d5f7a722237fa5cfdf26e35dede4714f9f8
Peter Vang <vang_peter06@yahoo.com>
2022-07-04 11:03:26 -0700

    create test two. return age in mercury year

--- __tests__/solarCal_test_.js
+++ __tests__/solarCal_test_.js
@@ -10,4 +10,16 @@ beforeEach(() => {
 test("should return the age of Earth years", () => {
   expect(newTest.earthCal()).toEqual(34);
 });
+
+
+test("should return the age of Mercury years", () => {
+  expect(newTest.mercuryCal()).toEqual(8);
+});
+
+
+
+
+
+
+
 });
