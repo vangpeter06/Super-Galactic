300794f0e452a30dc5bdcbb957df38b3e7884ddb
Peter Vang <vang_peter06@yahoo.com>
2022-07-04 13:18:09 -0700

    test two, should return life expectancy on user input for gender

--- __tests__/expectancy_test_.js
+++ __tests__/expectancy_test_.js
@@ -4,10 +4,16 @@ describe ('Expectancy', () => {
   let expectancyTest;
   beforeEach(() => {
     let region = "Americas";
+    let gender = "male";
     expectancyTest = new Expectancy(region);
   })
 
   test('should return life expectancy based on user input for region', ()=> {
     expect(expectancyTest.regionCal()).toEqual(79)
-  })
+  });
+
+  test('should return life expectancy based on user input for gender', ()=> {
+    expect(expectancyTest.regionCal()).toEqual(79)
+  });
+
 })
